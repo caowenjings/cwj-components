59e9f6e6b5d29b007228f94eda77d0969cdcac32
caowenjings <13508084+caowenjings@user.noreply.gitee.com>
2024-09-12 00:57:31 +0800

    解决father打包错误

--- .fatherrc.ts
+++ .fatherrc.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'father';
 
-export default defineConfig({
+export default ({
   // more father config: https://github.com/umijs/father/blob/master/docs/config.md
   esm: { output: 'dist' }, //输出路径
 });
